03c3ee3fb01f2df68e32cbf8f3418f34d4964635
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2022-02-01 10:40:02 +0000

    Add JSX static components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 
 function App() {
-  return <div>Getting there! :)</div>;
+  return (
+    <div>
+      <h1>My guests</h1>
+      <div>
+        <h2>Name</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
